864adfdbb726c60534c887117520ca61dc26daf8
“suyashkhanzode” <suyashkhanzode123@gmail.com>
2024-04-17 22:42:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next) =>{
+    console.log("Hello")
+})
+
+let server = http.createServer(app)
+
+server.listen(4000);

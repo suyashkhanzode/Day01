c88b8466d14093380a9e1d223783a86535678d2d
“suyashkhanzode” <suyashkhanzode123@gmail.com>
2024-04-23 23:05:43 +0530

    Intro to Middlewares

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const http = require("http");
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req,res,next) =>{
-    console.log("Hello")
-    next();
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/add-product',(req,res,next) =>{
+  res.send(`<form action = "/product" method ="POST"> Title: <input type ="text" name ="title"/> Size: <input type ="text" name ="size"/><button type = "submit">Add Product</button></form>`);
+    
 })
 
-app.use((req,res,next) =>{
-    console.log("World")
-})
+app.use('/product',(req,res,next) =>{
+    console.log(req.body);
+    
+  })
+
 
 
 
